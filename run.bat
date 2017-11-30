f78d039643d55bc74b85739ea35409fcd961a929
wudi <wudi19870903@163.com>
2017-11-30 11:21:09 +0800

    x

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 echo run.java.class [%1]
-java -Xms1024m -Xmx1024m -Xmn200m -cp QuantS1711.jar;dom4j-1.6.1.jar;DQuant.jar;dom4j-1.6.1.jar;htmlparser-1.6.jar;jeromq-0.3.4.jar;json-20170516.jar;protobuf-java-2.4.1.jar;servlet-api.jar;THSApi.jar;xmlParserAPIs-2.0.2.jar; %1
+java -Xms1536m -Xmx1536m -Xmn512m -Xss4m -cp QuantS1711.jar;dom4j-1.6.1.jar;DQuant.jar;dom4j-1.6.1.jar;htmlparser-1.6.jar;jeromq-0.3.4.jar;json-20170516.jar;protobuf-java-2.4.1.jar;servlet-api.jar;THSApi.jar;xmlParserAPIs-2.0.2.jar; %1
