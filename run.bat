03d696262a1577f4f813aa0040354204229c019e
wudi <wudi19870903@163.com>
2017-11-30 10:58:59 +0800

    x

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -cp QuantS1711.jar;dom4j-1.6.1.jar;DQuant.jar;dom4j-1.6.1.jar;htmlparser-1.6.jar;jeromq-0.3.4.jar;json-20170516.jar;protobuf-java-2.4.1.jar;servlet-api.jar;THSApi.jar;xmlParserAPIs-2.0.2.jar; strategy.QS1605Real
+java -Xms1024m -Xmx1024m -Xmn200m -cp QuantS1711.jar;dom4j-1.6.1.jar;DQuant.jar;dom4j-1.6.1.jar;htmlparser-1.6.jar;jeromq-0.3.4.jar;json-20170516.jar;protobuf-java-2.4.1.jar;servlet-api.jar;THSApi.jar;xmlParserAPIs-2.0.2.jar; strategy.QS1605Real
