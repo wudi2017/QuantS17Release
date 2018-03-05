2b3944d3b2499caf5a0aa5a263e448e3689f52ad
wudi <wudi19870903@163.com>
2018-03-05 11:45:45 +0800

    x

--- run.bat
+++ run.bat
@@ -1,2 +1,16 @@
+@echo off
+if "%1" == "" (goto help) else goto run
+
+:run
 echo run.java.class [%1]
 java -Xms1536m -Xmx1536m -Xmn512m -Xss4m -cp QuantS1711.jar;dom4j-1.6.1.jar;DQuant.jar;dom4j-1.6.1.jar;htmlparser-1.6.jar;jeromq-0.3.4.jar;json-20170516.jar;protobuf-java-2.4.1.jar;servlet-api.jar;THSApi.jar;xmlParserAPIs-2.0.2.jar; %1
+goto :end
+
+:help
+echo Param: run java class
+echo     class list:
+echo     strategy.QS1711.Main
+echo     strategy.QS1801.QS1802T1
+goto :end
+
+:end  
